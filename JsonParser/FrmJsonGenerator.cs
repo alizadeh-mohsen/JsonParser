989d11ccd1613c369ef966fbd5786ba1439cb51a
mohsen <mohsen@ojworld.co.kr>
2019-10-15 09:35:41 +0330

    Add project files.

---
+++ JsonParser/FrmJsonGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JsonParser
+{
+    public partial class FrmJsonGenerator : Form
+    {
+        public FrmJsonGenerator()
+        {
+            InitializeComponent();
+        }
+    }
+}
